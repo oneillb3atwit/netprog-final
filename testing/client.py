053bebb221478492513cf8390a9f118f6d77747d
Ben O'Neill <ben@oneill.sh>
2022-10-28 09:31:58 -0400

    simplify client

--- testing/client.py
+++ testing/client.py
@@ -4,15 +4,13 @@ import sys, pygame, socket, json
 HOST = 'localhost'
 PORT = 8001
 
-
 # PYGAME INIT
 pygame.init()
 size = width, height = 320, 240
-speed = [2, 2]
 black = 0, 0, 0
 screen = pygame.display.set_mode(size)
-ball = pygame.image.load('img.png')
-ballrect = ball.get_rect()
+img = pygame.image.load('img.png')
+imgrect = img.get_rect()
 clock = pygame.time.Clock()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -20,20 +18,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: sys.exit()
-		if ballrect.left < 0 or ballrect.right > width:
-			speed[0] = -speed[0]
-		if ballrect.top < 0 or ballrect.bottom > height:
-			speed[1] = -speed[1]
-	
+
 		data = None
 		s.send(b'give me a new x')
 		data = s.recv(1024)
 		data = str(data)[2:-1]
 		jdata = json.loads(str(data))
-		ballrect.x = jdata['x']
-	
+		imgrect.x = jdata['x']
+
 		screen.fill(black)
-	
-		screen.blit(ball, ballrect)
+		screen.blit(img, imgrect)
 		pygame.display.flip()
 		clock.tick(60)
