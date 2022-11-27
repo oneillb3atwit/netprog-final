c78467248f4c9e3bfedc32afc6d7ac5c942d05ad
Jeff Parla <parlaj@wit.edu>
2022-11-27 16:35:10 -0500

    air hockey collision and team fix need to fix the puck velocity and wall collision, bounds are broken

--- src/airhockey.py
+++ src/airhockey.py
@@ -4,10 +4,11 @@ from engine.server import *
 import pygame
 
 PLAYERSIZE = (64, 64)
-PLAYERSPEED = 8
-BALLSIZE = (64, 64)
-BALLSPEED = 4
+PUCKSIZE = (64, 64)
 GOALBOUNDS = (20, 100)
+PUCKMAXSPEED = 50
+
+
 
 class Player(DrawableObject):
     """
@@ -21,6 +22,10 @@ class Player(DrawableObject):
         x position of the player.
     y : int
         y position of the player.
+    x_vel : int
+        x velocity of the player (current x - prev x), server side
+    y_vel : int
+        y velocity of the player (current y - prev y), server side
     team : int
         the player's team (0: left, or 1: right).
     bounds : (int, int)
@@ -41,13 +46,15 @@ class Player(DrawableObject):
         data : dict
             Values of self.id, self.x, self.y, and self.team in a dict object
         """
+        self.type = 'Player'
         if data == None:
             self.x = 0
             self.y = 0
             self.team = 0
         else:
             self.client_update(data)
-
+        self.x_vel = 0
+        self.y_vel = 0
         self.sprite = pygame.image.load('img/airhockey/paddle.png')
         self.bounds = (self.x + PLAYERSIZE[0], self.y + PLAYERSIZE[1])
 
@@ -91,6 +98,8 @@ class Player(DrawableObject):
             the change in y value
         """
 
+        self.x_vel = self.x - pos[0]
+        self.y_vel = self.y - pos[1]
 
         if pos[0] <= (WINSIZE[0] / 2) * self.team:
             self.x = (WINSIZE[0] / 2) * self.team
@@ -125,10 +134,10 @@ class Puck(DrawableObject):
         The x value of the puck in the map
     y : int
         The y value of the puck in the map
-    x_direction : int
-        The x direction of the puck's movement (-1: left, 1: right)
-    y_direction : int
-        The y direction of the puck's movement (-1: up, 1: down)
+    x_vel : int
+        The x velocity of the puck (-1: left, 1: right)
+    y_vel : int
+        The y velocity of the puck (-1: up, 1: down)
     """
 
     def __init__(self, data=None):
@@ -140,12 +149,13 @@ class Puck(DrawableObject):
         data : dict
             pre-set values if retrieved from the server.
         """
+        self.type = 'Puck'
         self.sprite = pygame.image.load('img/airhockey/puck.png')
         if data == None:
-            self.x = 0
-            self.y = 0
-            self.x_direction = 1
-            self.y_direction = 1
+            self.x = 272
+            self.y = 272
+            self.x_vel = 0
+            self.y_vel = 0
         else:
             self.client_update(data)
 
@@ -173,28 +183,40 @@ class Puck(DrawableObject):
         """
         return {'type': 'Puck', 'x': self.x, 'y': self.y}
 
-    def handle_collision(self):
+    def handle_collision(self, game_objects):
         """
         Handles player and wall collision.
         """
-        if self.x + BALLSIZE[0] >= WINSIZE[0]:
-            self.x_direction = -1
-        if self.x <= 0:
-            self.x_direction = 1
-        if self.y + BALLSIZE[1] >= WINSIZE[1]:
-            self.y_direction = -1
-        if self.y <= 0:
-            self.y_direction = 1
-
+        # collision goes here :D
+        for g in game_objects:
+            if (g.type == 'Player'):
+                puckrect = pygame.Rect(self.x, self.y, PUCKSIZE[0], PUCKSIZE[1])
+                playerrect = pygame.Rect(g.x, g.y, PLAYERSIZE[0], PLAYERSIZE[1]) 
+                if (puckrect.colliderect(playerrect)):
+                    print('colliding')
+                    self.x_vel = max(min(PUCKMAXSPEED, -g.x_vel), -PUCKMAXSPEED)
+                    self.y_vel = max(min(PUCKMAXSPEED, -g.y_vel), -PUCKMAXSPEED)
+                    self.x += self.x_vel * 1.1
+                    self.y += self.y_vel * 1.1
+        if (self.x <= 0 or self.x >= WINSIZE[0]):
+            self.x_vel *= -1
+        if (self.y <= 0 or self.y >= WINSIZE[1]):
+            self.y_vel *= -1
+            
     def move(self):
         """
         Moves the puck based on the direction and collision status
         """
-        self.handle_collision()
-        self.x += self.x_direction * BALLSPEED
-        self.y += self.y_direction * BALLSPEED
+        self.x += self.x_vel * 1.1
+        self.y += self.y_vel * 1.1
 
-    def server_update(self, data):
+    def reset_puck(self):
+        self.x = 272
+        self.y = 272
+        self.x_vel = 0
+        self.y_vel = 0
+
+    def server_update(self, game_objects):
         """
         Runs the game logic on the server
         This is equivalent to an update function in a single player game
@@ -204,6 +226,7 @@ class Puck(DrawableObject):
         keys : list(str)
             The client's currently pressed keys
         """
+        self.handle_collision(game_objects)
         self.move()
 
 class AirHockeyClient(GameClient):
@@ -268,8 +291,40 @@ class AirHockeyServer(GameServer):
         self.clients.append(addr)
         player = Player()
         player.id = len(self.clients)
+        player.team = len(self.clients) % 2
         self.game_objects.append(player)
         return len(self.clients)
+    def game_loop(self):
+        """
+        The main server loop. Overriding to send server info to the puck object
+        """
+        self.running = True
+        while self.running:
+            data, addr = self.sock.recvfrom(1024)
+            game_objects_json = []
+            for o in self.game_objects:
+                game_objects_json.append(o.get_dict())
+
+            if data == None: break
+            data = str(data)[2:-1]
+            if data == CLIENT_CONNECT_MESSAGE:
+                client_id = self.add_client(addr)
+                response = bytes(json.dumps({'id': client_id, 'game_objects': game_objects_json}), encoding='utf-8')
+                self.sock.sendto(bytes(json.dumps({'id': client_id, 'game_objects': game_objects_json}), encoding='utf-8'), addr)
+                continue
+
+            try:
+                data_dict = json.loads(data)
+                client_id = data_dict['id']
+                for o in self.game_objects:
+                    if (o.type == 'Puck'):
+                        o.server_update(self.game_objects)
+                    else:
+                        o.server_update(data_dict)
+            except ValueError as e:
+                printd('Malformed packet received.')
+            self.sock.sendto(bytes(json.dumps({'id': client_id, 'game_objects': game_objects_json}), encoding='utf-8'), addr)
+        self.halt()
 
 args = parse_args()
 if 'debug' in args:
